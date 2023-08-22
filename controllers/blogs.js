af392b67b3c01a1e3a07127f8bc36699294afe7f
Adria Huertas <adriahc@gmail.com>
2023-08-22 15:13:05 +0200

    Fixed lint styles

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,15 @@
-import express from "express";
-import Blog from "../models/blog.js";
+import express from 'express';
+import Blog from '../models/blog';
 
 const blogsRouter = express.Router();
 
-blogsRouter.get("/", (request, response) => {
+blogsRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body);
 
   blog.save().then((result) => {
--- controllers/defaultRoutes.js
+++ controllers/defaultRoutes.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const defaultRoutesRouter = express.Router();
 
-defaultRoutesRouter.get("/", (request, response) => {
+defaultRoutesRouter.get('/', (request, response) => {
   response.send(
-    "<h1>API Blogs</h1> \
-  <p>This is a demo API build in express. <br>Main endpoint: /api/blogs</p>"
+    // eslint-disable-next-line comma-dangle
+    '<h1>API Blogs</h1><p>This is a demo API build in express. <br>Main endpoint: /api/blogs</p>'
   );
 });
 
