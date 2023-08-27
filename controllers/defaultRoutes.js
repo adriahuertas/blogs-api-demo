65939b16d27591c759e4e2c11d41245653f6b11f
Adria Huertas Caparros <adria@localhost.localdomain>
2023-08-27 11:28:08 +0200

    Added API routes to front page

--- controllers/defaultRoutes.js
+++ controllers/defaultRoutes.js
@@ -3,9 +3,15 @@ import express from 'express';
 const defaultRoutesRouter = express.Router();
 
 defaultRoutesRouter.get('/', (request, response) => {
-  response.send(
-    // eslint-disable-next-line comma-dangle
-    '<h1>API Blogs</h1><p>This is a demo API build in express. <br>Main endpoint: /api/blogs</p><h3>Author: Adrià Huertas</h3>'
+  response.send(`
+    <h1>API Blogs</h1>
+    <p>This is a demo API build in express. <br> 
+    Main endpoint: <br>
+    /api/blogs<br>
+    /api/blogs/id<br>
+    /api/users<br>
+    /api/users/id</p><h3>Author: Adrià Huertas</h3>
+  `
   );
 });
 
